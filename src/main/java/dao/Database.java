4522a5bb2c3708049cf11178f981e4cf7835e25f
zikei <sigetaka1517@gmail.com>
2022-07-20 20:22:48 +0900

    データベース変更

--- src/main/java/dao/Database.java
+++ src/main/java/dao/Database.java
@@ -1,6 +1,21 @@
 package dao;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class Database {
-	final String PASS="";
-	final String User="";
+	private final String JDBC_URL="";
+	private final String DB_USER="";
+	private final String DB_PASS="";
+	
+	protected boolean ConnectDatabase() {
+		//databaseにアクセスし、成功ならtrue　失敗ならfalseを返す
+		try(Connection conn = DriverManager.getConnection(JDBC_URL,DB_USER,DB_PASS)){
+		
+		}catch(SQLException e) {
+			return false;
+		}
+		return true;
+	}
 }
