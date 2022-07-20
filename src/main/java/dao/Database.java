12ce63aa6fc1df287828128d5f1ab570e0820c6a
zikei <sigetaka1517@gmail.com>
2022-07-20 23:58:58 +0900

    2022/7/20 11:59

--- src/main/java/dao/Database.java
+++ src/main/java/dao/Database.java
@@ -2,6 +2,6 @@ package dao;
 
 public class Database {
 	protected final String JDBC_URL="";
-	protected final String DB_USER="";
-	protected final String DB_PASS="";
+	protected final String DB_USER="examPlatform";
+	protected final String DB_PASS="amagasaki2022";
 }
