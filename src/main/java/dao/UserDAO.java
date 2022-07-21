de2c83a690a577d96c954136b256c6e69150ad58
zikei <sigetaka1517@gmail.com>
2022-07-21 23:17:13 +0900

    2022/7/21 11:17

--- src/main/java/dao/UserDAO.java
+++ src/main/java/dao/UserDAO.java
@@ -10,11 +10,16 @@ import model.Account;
 import model.User;
 
 public class UserDAO extends Database {
+	
+	final private String table = "UserTBL";
+	final private String UserID = "User_ID";
+	final private String PASS = "PASS";
+	
 	public User findByLogin(Account account) {
 		User user = null;
 		
 		try(Connection conn = DriverManager.getConnection(super.JDBC_URL, super.DB_USER, super.DB_PASS)){
-			String sql = "SELECT ID FROM UserID WHERE USER_ID = ? AND PASS = ?";
+			String sql = "SELECT " + UserID + " FROM " + table + " WHERE " + UserID + " = ? AND " + PASS + " = ?";
 			PreparedStatement pStmt = conn.prepareStatement(sql);
 			pStmt.setString(1, account.getUSER_ID());
 			pStmt.setString(2, account.getPASS());
@@ -33,5 +38,47 @@ public class UserDAO extends Database {
 		}
 		
 		return user;
+	}
+	
+	public boolean findByID(String id){
+		boolean resultSts=false;
+		
+		try(Connection conn = DriverManager.getConnection(super.JDBC_URL, super.DB_USER, super.DB_PASS)){
+			String sql = "SELECT " + UserID + " FROM " + table + " WHERE " + UserID + " = ?";
+			PreparedStatement pStmt = conn.prepareStatement(sql);
+			pStmt.setString(1, id);
+			
+			ResultSet rs = pStmt.executeQuery();
+			
+			if(rs.next()) {
+				resultSts=true;
+			}
+		}catch(SQLException e) {
+			e.printStackTrace();
+			return false;
+		}
+		
+		return resultSts;
+	}
+	
+	public boolean setAccount(Account account) {
+		boolean resultSts=false;
+		
+		try(Connection conn = DriverManager.getConnection(super.JDBC_URL, super.DB_USER, super.DB_PASS)){
+			String sql = "INSERT INTO " + table + "";
+			PreparedStatement pStmt = conn.prepareStatement(sql);
+			pStmt.setString(1, account.getUSER_ID());
+			pStmt.setString(2, account.getPASS());
+			
+			int result = pStmt.executeUpdate();
+			if(result>0) {
+				resultSts=true;
+			}
+		}catch(SQLException e) {
+			e.printStackTrace();
+			return false;
+		}
+		
+		return resultSts;
 	}
 }
