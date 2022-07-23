70934e192e5009785e1cfa8f3d90b54337da3600
zikei <sigetaka1517@gmail.com>
2022-07-23 23:08:10 +0900

    2022/7/23 11:08

--- src/main/java/dao/AccountDAO.java
+++ src/main/java/dao/AccountDAO.java
@@ -7,7 +7,6 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 
 import model.Account;
-import model.User;
 
 public class AccountDAO extends Database {
 	
@@ -15,8 +14,8 @@ public class AccountDAO extends Database {
 	final private String UserID = "UserID";
 	final private String PASS = "Password";
 	
-	public User findByLogin(Account account) {
-		User user = null;
+	public boolean isLogin(Account account) {
+		boolean resultSts=false;
 		
 		try(Connection conn = DriverManager.getConnection(super.JDBC_URL, super.DB_USER, super.DB_PASS)){
 			String sql = "SELECT " + UserID + " FROM " + table + " WHERE " + UserID + " = ? AND " + PASS + " = ?";
@@ -27,17 +26,14 @@ public class AccountDAO extends Database {
 			ResultSet rs = pStmt.executeQuery();
 			
 			if(rs.next()) {
-				String id = rs.getString("UserID");
-				user = new User(id,true);
-			}else {
-				user = new User(account.getUserID(),false);
+				resultSts=true;
 			}
 		}catch(SQLException e) {
 			e.printStackTrace();
-			return null;
+			return false;
 		}
 		
-		return user;
+		return resultSts;
 	}
 	
 	public boolean isID(String id){
