24b0f6a067e2091c1817bd3fe936db6a6b9fbed5
zikei <sigetaka1517@gmail.com>
2022-08-01 13:59:10 +0900

    2022/8/1 13:59

--- src/main/java/dao/BigQuestionDAO.java
+++ src/main/java/dao/BigQuestionDAO.java
@@ -3,16 +3,47 @@ package dao;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 
 import model.BigQuestion;
+import model.Question;
 
 public class BigQuestionDAO extends Database {
 	final private String TABLE = "BigQuestion";
-//	final private String EXAM_ID = "ExamID";
-//	final private String BIG_QUESTION_ID = "BigQuestionID";
-//	final private String BIG_QUESTION_SENTENCE = "BigQuestionSentence";
+	final private String EXAM_ID = "ExamID";
+	final private String BIG_QUESTION_ID = "BigQuestionID";
+	final private String BIG_QUESTION_SENTENCE = "BigQuestionSentence";
+	
+	public List<BigQuestion> findBigQuestion(String examID){
+		List<BigQuestion> bigQuestionList = new ArrayList<>();
+		QuestionDAO questionDAO = new QuestionDAO();
+		
+		try(Connection conn = DriverManager.getConnection(super.JDBC_URL, super.DB_USER, super.DB_PASS)){
+			String sql = "SELECT "+ BIG_QUESTION_ID + "," + BIG_QUESTION_SENTENCE + " FROM " + TABLE + 
+					" WHERE " + EXAM_ID + " = ?";
+			
+			PreparedStatement pStmt = conn.prepareStatement(sql);
+			pStmt.setString(1, examID);
+			
+			ResultSet rs = pStmt.executeQuery();
+			
+			while(rs.next()) {
+				int bigQuestionID = rs.getInt(BIG_QUESTION_ID);
+				String bigQuestionSentence = rs.getString(BIG_QUESTION_SENTENCE);
+				
+				List<Question> questionList = questionDAO.findQuestion(examID, bigQuestionID);
+						
+				bigQuestionList.add(new BigQuestion(examID, bigQuestionID, bigQuestionSentence, questionList));
+			}
+		}catch(SQLException e) {
+			e.printStackTrace();
+			return null;
+		}
+		return bigQuestionList;
+	}
 	
 	public boolean setQuestion(List<BigQuestion> bigQuestionList) {
 		if(bigQuestionList == null || bigQuestionList.isEmpty()) {
