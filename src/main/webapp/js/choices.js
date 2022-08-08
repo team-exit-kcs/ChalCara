c1dcdfe953766141bba2f10c5f852f7ed8d26ea9
zikei <sigetaka1517@gmail.com>
2022-08-08 13:40:43 +0900

    2022/8/8 13:40

--- src/main/webapp/js/choices.js
+++ src/main/webapp/js/choices.js
@@ -1,18 +1,18 @@
 function addChoicesForm(select){
-	let choicesValue = select.parentNode.parentNode.childElementCount-1;
-	let choicesCnt = choicesValue+1;
-	let choicesHtml = '<li class = "Select"><label><input type="radio" name="Select_Symbol" value="'+choicesValue+'"><span>'+choicesCnt+'．</span></label><input type="text" class="Select_text" name="Select_text" /><button type="button" onclick="rmChoicesForm(this)">−選択肢を削除</button></li>';
+	let choicesCnt = select.parentNode.parentNode.childElementCount;
+	let choicesHtml = '<li class = "Select"><label><input type="radio" name="Select_Symbol" value="'+choicesCnt+'"><span>'+choicesCnt+'．</span></label><input type="text" class="Select_text" name="Select_text" /><button type="button" onclick="rmChoicesForm(this)">−選択肢を削除</button></li>';
 	
 	select.parentNode.insertAdjacentHTML("beforebegin",choicesHtml);
 }
 
 function rmChoicesForm(rmChoices){
-	let choicesCnt = rmChoices.parentNode.parentNode.childElementCount-2;
+	let choicesCnt = rmChoices.parentNode.parentNode.childElementCount-1;
 	let Choices=rmChoices.parentNode;
 	for(let i=Number(Choices.firstElementChild.firstElementChild.value);i<choicesCnt;i++){
+		console.log(i);
 		Choices=Choices.nextElementSibling;
 		Choices.firstElementChild.firstElementChild.setAttribute('value',i);;
-		Choices.firstElementChild.lastElementChild.textContent = (i+1)+"．";
+		Choices.firstElementChild.lastElementChild.textContent = (i)+"．";
 	}
 	li = rmChoices.parentNode;
 	li.remove();
