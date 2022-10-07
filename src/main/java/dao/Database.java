228876b33b65273979e19f397604e774e42b1152
zikei <sigetaka1517@gmail.com>
2022-10-08 03:41:58 +0900

    db変更

--- src/main/java/dao/Database.java
+++ src/main/java/dao/Database.java
@@ -1,7 +1,7 @@
 package dao;
 
 public class Database {
-	protected final String JDBC_URL="jdbc:mysql://153.125.225.163/examplatform";
+	protected final String JDBC_URL="jdbc:mysql://localhost/examplatform";
 	protected final String DB_USER="examPlatform";
 	protected final String DB_PASS="Saruwatari@2022";
 }
